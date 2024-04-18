28f08c3327f77c1ae6e781baca3278f362a431d3
Quentin Fouilloux <fouillouxquentin@gmail.com>
2024-04-18 11:18:06 +0200

    Affichage de la modale au centre de l'écran

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,13 +1,12 @@
-const openModal = function (e) {
-    e.preventDefault();
-    const target = document.querySelector(e.target.getAttribute('href'));
+const openModal = function (event) {
+    event.preventDefault();
+    const target = document.querySelector(event.target.getAttribute('href'));
     target.style.display = null;
     target.removeAttribute('aria-hidden');
     target.setAttribute('aria-modal', 'true');
-
 };
 
-document.querySelectorAll('js-modal').forEach(a => {
+// Sélection des éléments avec la classe 'js-modal' et ajout d'un écouteur d'événements à chacun
+Array.from(document.querySelectorAll('.js-modal')).forEach(a => {
     a.addEventListener('click', openModal);
-    
 });
